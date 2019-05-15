70857c4909daf913cae1841979efea9dfef0449b
Dongbo <dongbo.hu@gmail.com>
2019-05-15 14:39:01 -0400

    tweak

--- .circleci/install_cimr.sh
+++ .circleci/install_cimr.sh
@@ -8,21 +8,21 @@ cd cimr
 # https://discuss.circleci.com/t/installing-git-lfs/867/11
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get update
-sudo apt-get install git-lfs
+sudo apt-get install git-lfs --yes --quiet
 #git lfs install
 git lfs pull
 
 python3 setup.py build
 sudo python3 setup.py install
 
-echo
+echo; echo "df -h"
 df -h
 
-echo
+echo; echo "free -g"
 free -g
 
-echo
+echo; echo "free -gt"
 free -gt
 
-echo
+echo; echo "vmstat -s"
 vmstat -s
