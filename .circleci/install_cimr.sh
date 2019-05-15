aa08c59d75b397b2fd73af561fbb35f5f93eb2a1
Dongbo <dongbo.hu@gmail.com>
2019-05-15 11:42:03 -0400

    Install git-lfs

--- .circleci/install_cimr.sh
+++ .circleci/install_cimr.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-cd ~/cimr-d
 git clone https://github.com/greenelab/cimr.git
 
 cd cimr
+
+# dhu: install git lfs
+# https://discuss.circleci.com/t/installing-git-lfs/867/11
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get update
+sudo apt-get install git-lfs
+git lfs install
+ssh git@github.com git-lfs-authenticate greenelab/cimr.git download
+
+
 
 git lfs pull
 
