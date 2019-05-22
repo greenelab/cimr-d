38bc390fca7d54660b7e5351c758314ae92de8b1
Dongbo <dongbo.hu@gmail.com>
2019-05-22 12:20:40 -0400

    pamd config

--- .circleci/install_cimr.sh
+++ .circleci/install_cimr.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+cd;
+mkdir git-lfs
+cd git-lfs
+wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.2/git-lfs-linux-386-v2.7.2.tar.gz
+tar xzf git-lfs-linux-386-v2.7.2.tar.gz
+mv git-lfs-linux-386-v2.7.2.tar.gz /tmp/
+sudo ./install.sh
+
 cd; git clone https://github.com/greenelab/cimr.git
 
 cd cimr
 
 # dhu: install git lfs
 # https://discuss.circleci.com/t/installing-git-lfs/867/11
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get update
-sudo apt-get install git-lfs --yes --quiet
+#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+#sudo apt-get update
+#sudo apt-get install git-lfs --yes --quiet
 #git lfs install
+
 git lfs pull
 
 python3 setup.py build
