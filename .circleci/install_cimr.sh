4435b0ecd65520c66b67123a2d88ae905feefae2
Dongbo <dongbo.hu@gmail.com>
2019-05-22 12:49:14 -0400

    Tweak

--- .circleci/install_cimr.sh
+++ .circleci/install_cimr.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-cd;
-mkdir git-lfs
-cd git-lfs
-wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.2/git-lfs-linux-386-v2.7.2.tar.gz
-tar xzf git-lfs-linux-386-v2.7.2.tar.gz
-mv git-lfs-linux-386-v2.7.2.tar.gz /tmp/
+# Download git-lfs tarball file in /tmp
+cd /tmp && wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.2/git-lfs-linux-386-v2.7.2.tar.gz
+
+# Unzip git-lfs tarball file in ~/git-lfs
+mkdir ~/git-lfs && cd ~/git-lfs
+tar xzf /tmp/git-lfs-linux-386-v2.7.2.tar.gz
 sudo ./install.sh
 
-cd; git clone https://github.com/greenelab/cimr.git
-
-cd cimr
-
-# dhu: install git lfs
-# https://discuss.circleci.com/t/installing-git-lfs/867/11
-#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-#sudo apt-get update
-#sudo apt-get install git-lfs --yes --quiet
-#git lfs install
-
-git lfs pull
-
+# Install cimr
+cd && git clone https://github.com/greenelab/cimr.git
+cd ~/cimr
+git lfs install && git lfs pull
 python3 setup.py build
 sudo python3 setup.py install
 
