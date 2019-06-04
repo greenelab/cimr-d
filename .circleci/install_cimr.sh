e0f1bba2cd14b3da3d3fbcbebb2261ee02712dd3
Dongbo <dongbo.hu@gmail.com>
2019-06-04 17:27:51 -0400

    Combine commands in one line

--- .circleci/install_cimr.sh
+++ .circleci/install_cimr.sh
@@ -14,7 +14,6 @@ sudo ./install.sh
 cd ~/
 git clone https://github.com/greenelab/cimr.git
 cd cimr
-git lfs install
-git lfs pull
+git lfs install && git lfs pull
 python3 setup.py build
 sudo python3 setup.py install
