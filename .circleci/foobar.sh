2d3bee0ece277c4c3265f7b449ab687dc22dad3e
Dongbo <dongbo.hu@gmail.com>
2019-06-05 10:30:19 -0400

    test git push

--- .circleci/foobar.sh
+++ .circleci/foobar.sh
@@ -6,6 +6,11 @@ git config --global user.email "cimrroot@gmail.com"
 git config --global user.name "cimrroot"
 git config --global push.default simple
 
+(date; echo "hello from cimrroot") >> foobar.txt
+git add foobar.txt
+git commit -m "Committed by CircleCI [skip ci]"
+git push
+
 #git add processed_data/*
 #git commit -m "Committed by CircleCI [skip ci]"
 #git push --force --quiet origin master
