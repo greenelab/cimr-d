b2c83b877ccd02b8f7f56e1db0758dd9c2e28ea5
Dongbo <dongbo.hu@gmail.com>
2019-06-05 11:18:08 -0400

    Clean up foobar.sh

--- .circleci/foobar.sh
+++ .circleci/foobar.sh
@@ -6,11 +6,7 @@ git config --global user.email "cimrroot@gmail.com"
 git config --global user.name "cimrroot"
 git config --global push.default simple
 
-(date; echo "hello from cimrroot") >> foobar.txt
+echo "`date`: hello from cimrroot" >> foobar.txt
 git add foobar.txt
 git commit -m "Committed by CircleCI [skip ci]"
 git push --set-upstream origin dhu/git-lfs
-
-#git add processed_data/*
-#git commit -m "Committed by CircleCI [skip ci]"
-#git push --force --quiet origin master
