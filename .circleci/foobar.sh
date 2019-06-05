04de07f3f66c2c77ff15684d817a66b311ecc4c4
Dongbo <dongbo.hu@gmail.com>
2019-06-05 10:38:50 -0400

    test foobar 4

--- .circleci/foobar.sh
+++ .circleci/foobar.sh
@@ -9,7 +9,7 @@ git config --global push.default simple
 (date; echo "hello from cimrroot") >> foobar.txt
 git add foobar.txt
 git commit -m "Committed by CircleCI [skip ci]"
-git push
+git push --set-upstream origin dhu/git-lfs
 
 #git add processed_data/*
 #git commit -m "Committed by CircleCI [skip ci]"
