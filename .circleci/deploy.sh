0e171084209f26b635baa7f73930094f01e5ae59
dongbohu <dongbo.hu@gmail.com>
2021-01-21 22:09:20 -0500

    Remove unnecessary command [skip ci]

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -54,9 +54,6 @@ if [ ! -f ${INDICATOR_FIELNAME} ]; then
     exit 0
 fi
 
-# Use the latest "pip"
-sudo pip install --upgrade pip
-
 # Install Google Cloud SDK
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
     | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
